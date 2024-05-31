845305ccc1c5e8680388c29caf60d121d5bf4962
nrjiz <164118742+nrjiz@users.noreply.github.com>
2024-05-31 11:52:44 +0300

    Create BJP5 Exercise 18.4: removeAllHashIntSet.java

---
+++ BJP5 Exercise 18.4: removeAllHashIntSet.java
@@ -0,0 +1,11 @@
+public void removeAll(HashIntSet set){
+    
+    for(int i =0;i<set.elementData.length;i++){
+        
+        Node current = set.elementData[i];
+        while(current!=null){           
+            remove(current.data);
+            current=current.next;
+        }
+    }
+}
